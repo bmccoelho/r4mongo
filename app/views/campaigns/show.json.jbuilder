4d9b01311b7ca587a9c18e95fd17b5a689533a5e
Bruno Coelho <bruno.coelho@gmail.com>
2014-01-09 09:35:07 +0000

    adds campaigns scaffold

---
+++ app/views/campaigns/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @campaign, :id, :name, :status, :created_at, :updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 R4mongo::Application.routes.draw do
+  resources :campaigns
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
